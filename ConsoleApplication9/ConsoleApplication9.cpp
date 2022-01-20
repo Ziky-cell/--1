ecf383de6eb612f0643a230904fdc1f4b4e5cbfa
Ziky-cell <va.fa.94@inbox.ru>
2022-01-20 15:59:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,24 @@
+﻿ #include <iostream>
+
+using namespace std;
+
+int faktorial(int n) {
+    int out = 1;
+    for (int i = 1; i <= n; i++)
+        out *= i;
+    return out;
+}
+
+int main()
+{
+    setlocale(0, "");
+    int n;
+    cout <<  "Введите n :";
+    cin >> n;
+    int k;
+    cout <<  "Введите k :";
+    cin >> k;
+    cout << "Ответ :";
+    cout << faktorial(n) * faktorial(k) * faktorial(n - k);
+    return 0;
+}
